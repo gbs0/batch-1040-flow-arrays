0eba67a1c4c22afb2753a44baec5bd1794d77a8d
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-12 10:44:13 -0300

    automatted commit

--- array.rb
+++ array.rb
@@ -13,19 +13,21 @@ beatles.push("Marlon Brando")
 beatles.push("Paul Walker")
 beatles.push("Brad Pitt")
 beatles.push("Arnorld Schwazenegger")
-beatles.delete_at(0)
-beatles.delete_at(-1)
+
+beatles.delete_at(0) # Removendo o PRIMEIRO elemento da lista
+beatles.delete_at(beatles.length - 1) # Removendo o ÚLTIMO elemento da lista
+beatles.delete_at(-1) # Removendo o ÚLTIMO elemento da lista com -1 
 
 # Iterando sobre a array p/ imprimir os nomes
 for names in beatles
     puts names
 end
 
-# Iterando sobre a array p/ imprimir os nomes
+# Iterando sobre a array p/ imprimir os nomes (bloco de várias linhas)
 beatles.each do |name|
   puts name
 end
 
-beatles.each { |element| puts element } # Iterando sobre a array p/ imprimir os nomes
+beatles.each { |element| puts element } # Iterando sobre a array p/ imprimir os nomes (bloco em apenas uma linha)
 
 
